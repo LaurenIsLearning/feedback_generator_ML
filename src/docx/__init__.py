322b0b7b4d2a5f1de5e33924de7a6978d22fc92b
Joshua Carlson <jrcarl624@gmail.com>
2025-03-19 19:56:46 -0400

    feat: add todo, models dir, and docx stubs

---
+++ src/docx/__init__.py
@@ -0,0 +1,38 @@
+from spire.doc import *
+from spire.doc.common import *
+
+from .comments import parse_comments, Comments
+from .submission import parse_submission, Submission
+from .requirements import parse_requirements, Requirements
+from .rubric import parse_rubric, Rubric
+
+
+class StudentSubmission:
+
+    rubric: Rubric
+    """
+    The rubric table at the bottom of the submission document 
+    """
+
+    comments: Comments
+    """
+    The inline comments 
+    """
+
+    requirements: Requirements
+    """
+    The requirements file 
+    """
+
+    submission: Submission
+    """
+    The students written work
+    """
+
+    def __init__(self, submission: Document, requirements: Requirements) -> None:
+        self.rubric = parse_rubric(submission)
+        self.submission = parse_submission(submission)
+        self.comments = parse_comments(submission)
+        self.requirements = requirements
+
+    # TODO: Make getters and setters
---
+++ src/docx/requirements.py
@@ -0,0 +1,16 @@
+from spire.doc import *
+from spire.doc.common import *
+
+
+class Requirments:
+
+    # Put the fields for this class here
+
+    def __init__(self) -> None:
+        pass
+
+    # Define getters and setters, make the setters return self
+
+
+def parse_requirements(requirments: Document) -> Requirments:
+    pass
