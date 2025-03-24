253b4fc9fa6dcb6a3f159123ecc42b8503ada682
LaurenIsLearning <pleasecontactlauren@gmail.com>
2025-03-23 21:43:58 -0400

    uploaded __main__.py and renamed assignment.docx to requirement.docx

---
+++ __main__.py
@@ -0,0 +1,23 @@
+from spire.doc import *
+from spire.doc.common import *
+
+# NOTE: The dot notates its within the current directory
+from tropos.ui import make_ui
+from tropos.preprocess_docx import StudentSubmission
+
+
+# Starts the program
+if __name__ == "__main__":
+    # Tests the ui
+    # make_ui()
+
+    # Tests the docx data extraction
+
+    requirements_doc = Document()
+    requirements_doc.LoadFromFile("./data/sample1/AssignmentRequirements.docx")
+
+    submission_doc = Document()
+    requirements_doc.LoadFromFile("./data/sample1/StudentSubmission.docx")
+
+    print("Testing StudentSubmission class")
+    StudentSubmission(submission_doc, requirements_doc)
