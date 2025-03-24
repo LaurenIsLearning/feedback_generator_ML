71f7734d4a9616875e891d632fc5d12c2b596219
Joshua Carlson <61999256+jrcarl624@users.noreply.github.com>
2025-03-23 22:02:38 -0400

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,23 +1,4 @@
-from spire.doc import *
-from spire.doc.common import *
+from tropos import main
 
-# NOTE: The dot notates its within the current directory
-from tropos.ui import make_ui
-from tropos.preprocess_docx import StudentSubmission
-
-
-# Starts the program
 if __name__ == "__main__":
-    # Tests the ui
-    # make_ui()
-
-    # Tests the docx data extraction
-
-    requirements_doc = Document()
-    requirements_doc.LoadFromFile(".data/raw/Requirements.docx")
-
-    submission_doc = Document()
-    submission_doc.LoadFromFile("data/raw/Student 1/Student 1 Part 1.docx")
-
-    print("Testing StudentSubmission class")
-    StudentSubmission(submission_doc, requirements_doc)
+    main()
