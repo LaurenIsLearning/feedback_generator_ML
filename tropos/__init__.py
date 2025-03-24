223e1e46d07c8396a17a8e3f9038dfe311a3cb64
Joshua Carlson <61999256+jrcarl624@users.noreply.github.com>
2025-03-23 22:03:30 -0400

    Update __init__.py

--- tropos/__init__.py
+++ tropos/__init__.py
@@ -7,6 +7,28 @@ from .preprocess_docx.rubric import parse_rubric
 from .preprocess_docx.submission import parse_submission
 from .preprocess_docx.requirements import parse_requirements
 from .preprocess_docx.comments import parse_comments
+from .preprocess_docx import StudentSubmission
 
 # Optional UI export
 from .gradio.ui import launch_ui
+
+from spire.doc import *
+from spire.doc.common import *
+
+
+
+# Starts the program
+def main():
+    # Tests the ui
+    # make_ui()
+
+    # Tests the docx data extraction
+
+    requirements_doc = Document()
+    requirements_doc.LoadFromFile(".data/raw/Requirements.docx")
+
+    submission_doc = Document()
+    submission_doc.LoadFromFile("data/raw/Student 1/Student 1 Part 1.docx")
+
+    print("Testing StudentSubmission class")
+    StudentSubmission(submission_doc, requirements_doc)
