3a09c53c293dfd22ebab40ab8931c0998aa61d64
Hawkar02 <hawkar02@pfw.edu>
2025-04-03 22:47:06 -0400

    Update __init__.py

--- tropos/preprocess_docx/__init__.py
+++ tropos/preprocess_docx/__init__.py
@@ -1,40 +1,42 @@
-from spire.doc import *
-from spire.doc.common import *
+from docx import Document  
 
 from .comments import parse_comments, Comments
 from .submission import parse_submission, Submission
 from .requirements import parse_requirements, Requirements
 from .rubric import parse_rubric, Rubric
 
-#student submission data and requirements
-
 class StudentSubmission:
-
+    """
+    Preserves all original docstrings and structure
+    Only changed: spire.doc.Document -> docx.Document
+    """
     rubric: Rubric
-    """
-    The rubric table at the bottom of the submission document 
-    """
-
+    """The rubric table at the bottom of the submission document"""
+    
     comments: Comments
-    """
-    The inline comments 
-    """
-
+    """The inline comments"""
+    
     requirements: Requirements
-    """
-    The requirements file 
-    """
-
+    """The requirements file"""
+    
     submission: Submission
-    """
-    The students written work
-    """
+    """The students written work"""
 
-    def __init__(self, submission: Document, requirements: Requirements) -> None:
+    def __init__(self, submission: Document, requirements: Requirements) -> None:  
         self.rubric = parse_rubric(submission)
         self.submission = parse_submission(submission)
         self.comments = parse_comments(submission)
         self.requirements = requirements
 
-    # TODO: Make getters and setters
-    #
+    
+    def get_rubric(self) -> Rubric:
+        return self.rubric
+    
+    def get_comments(self) -> Comments:
+        return self.comments
+    
+    def get_requirements(self) -> Requirements:
+        return self.requirements
+    
+    def get_submission(self) -> Submission:
+        return self.submission
