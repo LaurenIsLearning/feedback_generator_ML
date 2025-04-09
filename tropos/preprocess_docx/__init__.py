31a24f94bc92d21dda3cb91d7e85a48693b095bd
Conner Reiter <connerr1978@gmail.com>
2025-04-09 11:15:10 -0400

    WIP-file-structure-and-output
    adjust the file stucture of the output, and how the data documents are made

--- tropos/preprocess_docx/__init__.py
+++ tropos/preprocess_docx/__init__.py
@@ -1,8 +1,7 @@
-# from spire.doc import *
-# from spire.doc.common import *
+
 from docx import Document
 
-from .comments import parse_comments, Comments
+from .comments import Comments
 from .submission import parse_submission, Submission
 from .assignment_requirements import parse_requirements, AssignmentRequirements
 from .rubric import parse_rubric, Rubric
@@ -10,33 +9,20 @@ from .rubric import parse_rubric, Rubric
 #student submission data and requirements
 
 class StudentSubmission:
-
-    rubric: Rubric
-    """
-    The rubric table at the bottom of the submission document 
-    """
-
-    comments: Comments
-    """
-    The inline comments 
-    """
-
-    requirements: AssignmentRequirements
-    """
-    The requirements file 
-    """
-
-    submission: Submission
-    """
-    The students written work
-    """
-
     def __init__(self, submission_path: str, requirements_path: str) -> None:
         self.rubric = parse_rubric(submission_path)
-        self.submission = parse_submission(submission_path)
-        self.comments = parse_comments(submission_path)
-        self.assignment_requirements = parse_requirements(Document(requirements_path))
-        
+        self.submission = parse_submission(Document(submission_path))
+        self.comments = Comments(submission_path).parse_comments()
+        self.assignment_requirements = parse_requirements(requirements_path)
 
+    def to_dict(self):
+        return {
+            'comments': self.comments.get_results(),
+            'rubric': self.rubric.get_criteria(),
+            'feedback': self.rubric.get_comments(),
+            'assignment_requirements': self.assignment_requirements.get_instructions(),
+            'submission_text': self.submission.get_content()
+        }
+    
     # TODO: Make getters and setters
     #
